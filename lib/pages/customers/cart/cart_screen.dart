e89b316255d8d7911efb22f65b5d75b7bcb6caef
kientrienhong <kientrienhong@gmail.com>
2021-11-27 23:31:16 +0700

    finish UI cart tab

--- lib/pages/customers/cart/cart_screen.dart
+++ lib/pages/customers/cart/cart_screen.dart
@@ -1,15 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:rssms/common/custom_color.dart';
+import 'package:rssms/pages/customers/cart/widgets/cart_tab.dart';
 
-class CartScreen extends StatelessWidget {
+class CartScreen extends StatefulWidget {
   const CartScreen({Key? key}) : super(key: key);
 
   @override
+  State<CartScreen> createState() => _CartScreenState();
+}
+
+class _CartScreenState extends State<CartScreen> {
+  late int _index;
+
+  @override
+  void initState() {
+    super.initState();
+    _index = 0;
+  }
+
+  void tapTab(int index) {
+    setState(() {
+      _index = index;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    final deviceSize = MediaQuery.of(context).size;
+
+    return Scaffold(
       backgroundColor: CustomColor.white,
-      body: Center(
-        child: Text('Home'),
+      body: Stack(
+        children: [
+          CartTab(deviceSize: deviceSize, index: _index, tapTab: tapTab)
+        ],
       ),
     );
   }
---
+++ lib/pages/customers/cart/widgets/cart_tab.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:rssms/common/custom_color.dart';
+
+class CartTab extends StatelessWidget {
+  final Size deviceSize;
+  final int index;
+  final Function(int) tapTab;
+  const CartTab(
+      {Key? key,
+      required this.deviceSize,
+      required this.index,
+      required this.tapTab})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: Alignment.bottomCenter,
+      child: Padding(
+        padding: EdgeInsets.only(
+            bottom: 16,
+            left: deviceSize.width / 7,
+            right: deviceSize.width / 7),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(24),
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.transparent,
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.1),
+                  spreadRadius: 3,
+                  blurRadius: 16,
+                  offset: Offset(0, 0), // changes position of shadow
+                ),
+              ],
+            ),
+            child: Theme(
+              data: Theme.of(context).copyWith(canvasColor: Colors.transparent),
+              child: BottomNavigationBar(
+                selectedItemColor: CustomColor.purple,
+                backgroundColor: CustomColor.white,
+                type: BottomNavigationBarType.fixed,
+                currentIndex: index,
+                onTap: tapTab,
+                items: const <BottomNavigationBarItem>[
+                  BottomNavigationBarItem(
+                      icon: ImageIcon(
+                        AssetImage('assets/images/box.png'),
+                      ),
+                      label: 'Giữ đồ thuê'),
+                  BottomNavigationBarItem(
+                      icon: ImageIcon(
+                        AssetImage('assets/images/warehouse.png'),
+                      ),
+                      label: 'Kho tự quản'),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
