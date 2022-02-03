2f8f1974130e8bba10e9f0af62894426ee6208aa
kientrienhong <kientrienhong@gmail.com>
2022-02-03 21:31:54 +0700

    notifcation for delivery staff

--- lib/pages/delivery_staff/notifcation/notification_delivery.dart
+++ lib/pages/delivery_staff/notifcation/notification_delivery.dart
@@ -1,16 +1,50 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:rssms/common/custom_color.dart';
 import 'package:rssms/common/notifcation_widget.dart';
 import 'package:rssms/constants/constants.dart' as constants;
+import 'package:rssms/models/entity/notification.dart';
+import 'package:rssms/models/entity/user.dart';
+import 'package:rssms/models/notification_screen_model.dart';
+import 'package:rssms/presenters/notification_screen_presenter.dart';
+import 'package:rssms/views/notification_screen_view.dart';
 
-class NotificationDeliveryScreen extends StatelessWidget {
+class NotificationDeliveryScreen extends StatefulWidget {
   const NotificationDeliveryScreen({Key? key}) : super(key: key);
 
-  List<Widget> mapNotifcationWidget(listNotification) => listNotification
-      .map<NotificationWidget>((p) => NotificationWidget(
-            notification: p,
-          ))
-      .toList();
+  @override
+  State<NotificationDeliveryScreen> createState() =>
+      _NotificationDeliveryScreenState();
+}
+
+class _NotificationDeliveryScreenState extends State<NotificationDeliveryScreen>
+    implements NotificationScreenView {
+  late NotificationScreenModel _model;
+  late NotificationScreenPresenter _presenter;
+
+  List<Widget> mapNotifcationWidget(
+          List<NotificationEntity> listNotification) =>
+      listNotification
+          .map<NotificationWidget>((p) => NotificationWidget(
+                notification: p,
+              ))
+          .toList();
+
+  @override
+  void initState() {
+    _presenter = NotificationScreenPresenter();
+    _presenter.view = this;
+    _model = _presenter.model;
+    Users user = Provider.of<Users>(context, listen: false);
+    _presenter.loadListNotification(user);
+    super.initState();
+  }
+
+  @override
+  void updateView() {
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,7 +55,7 @@ class NotificationDeliveryScreen extends StatelessWidget {
         child: ListView(
           padding: const EdgeInsets.all(8),
           shrinkWrap: true,
-          children: mapNotifcationWidget(constants.LIST_NOTIFICATION_DELIVERY),
+          children: mapNotifcationWidget(_model.list),
         ),
       ),
     );
