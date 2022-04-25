4b3b822902ec90cc1cdf7d503fdc79ba88f06e0b
GDante <dinhgiang443@gmail.com>
2022-04-25 19:56:12 +0700

    Update margin choice storage

--- lib/pages/customers/choice_storage/widgets/storage_choice.dart
+++ lib/pages/customers/choice_storage/widgets/storage_choice.dart
@@ -27,6 +27,7 @@ class _StorageChoiceWidgetState extends State<StorageChoiceWidget> {
   Widget build(BuildContext context) {
     final deviceSize = MediaQuery.of(context).size;
     return Container(
+      margin: EdgeInsets.symmetric(vertical: 8),
       padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
       decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(8),
